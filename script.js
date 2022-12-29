ed3269facac6e29d05a115fa080ac4275b045348
Patryk Kulbinski <100892074+Kulbikk@users.noreply.github.com>
2022-12-29 20:15:47 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -5,30 +5,41 @@ const addText3 = document.getElementById('add-text3');
 const addText4 = document.getElementById('add-text4');
 const addText5 = document.getElementById('add-text5');
 
-const trash1 = document.getElementById('trash1');
-const trash2 = document.getElementById('trash2');
-const trash3 = document.getElementById('trash3');
-const trash4 = document.getElementById('trash4');
-const trash5 = document.getElementById('trash5');
 
 const inputField = document.getElementById('inputt');
 
 button.addEventListener('click', () => {
   if (addText.textContent === '') {
     addText.textContent = inputField.value;
-    trash1.style.display = 'block';
   } else if (addText2.textContent === '') {
     addText2.textContent = inputField.value;
-    trash2.style.display = 'block';
   } else if (addText3.textContent === '') {
     addText3.textContent = inputField.value;
-    trash3.style.display = 'block';
   } else if (addText4.textContent === '') {
     addText4.textContent = inputField.value;
-    trash4.style.display = 'block';
   } else if (addText5.textContent === '') {
     addText5.textContent = inputField.value;
-    trash5.style.display = 'block';
   }
   inputField.value = '';
 });
+
+
+addText.addEventListener('click', () => {
+  addText.textContent = '';
+})
+
+addText2.addEventListener('click', () => {
+  addText2.textContent = '';
+})
+
+addText3.addEventListener('click', () => {
+  addText3.textContent = '';
+})
+
+addText4.addEventListener('click', () => {
+  addText4.textContent = '';
+})
+
+addText5.addEventListener('click', () => {
+  addText5.textContent = '';
+})
